510e2a1ebeebc9734506d63ab00fc50a76371abc
Maksim Gritsuk <megahappy@bk.ru>
2025-05-18 13:36:06 +0300

    Сделал определитель платформы

--- Assets/Game/Scripts/MainMenu.cs
+++ Assets/Game/Scripts/MainMenu.cs
@@ -5,6 +5,17 @@ using YG;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject tablet;
+    [SerializeField] private GameObject portrait;
+
+    private void Start()
+    {
+        portrait.SetActive(false);
+        tablet.SetActive(false);
+        
+        if (YG2.envir.isMobile) portrait.SetActive(true); 
+        else tablet.SetActive(true);
+    }
 
     public void NewGame()
     {
